9a2c6c902f6dacddfd66e0e812c1e928c74d4ab7
juanprm24 <jprodriguezmontero@gmail.com>
2025-10-14 21:45:07 -0600

    Seccion de estadisticas y cambios en inicio

--- Sportine/app/src/main/java/com/example/sportine/MainActivity.java
+++ Sportine/app/src/main/java/com/example/sportine/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.sportine;
 
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.sportine.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,19 +16,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Configuración de los destinos principales
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_stats
+        ).build();
+
+        NavController navController = Navigation.findNavController(
+                this,
+                R.id.nav_host_fragment_activity_main
+        );
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
